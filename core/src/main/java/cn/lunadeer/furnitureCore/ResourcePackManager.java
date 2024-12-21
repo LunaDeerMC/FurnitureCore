2fb09dc4531a737bc373fba64e15d968ca571b32
zhangyuheng <zhangyuheng@lunadeer.cn>
2024-12-21 19:55:53 +0800

    modify structure

--- core/src/main/java/cn/lunadeer/furnitureCore/ResourcePackManager.java
+++ core/src/main/java/cn/lunadeer/furnitureCore/ResourcePackManager.java
@@ -2,16 +2,24 @@ package cn.lunadeer.furnitureCore;
 
 import java.io.File;
 
-public class GenerateResourcePack {
+public class ResourcePackManager {
 
-    // 1. prepare resource_pack directory in cache directory (cache/resource_pack)
 
-    // 2. copy pre-defined files to the resource_pack directory
-    //      - pack.mcmeta (need to rename from pack.mcmeta.json)
-    //      - pack.png
-    //      - assets (dir)
 
-    // 3. save all models (get from ModelManager)
+    public ResourcePackManager() {
+
+    }
+
+    public void generate() {
+        // 1. prepare resource_pack directory in cache directory (cache/resource_pack)
+
+        // 2. copy pre-defined files to the resource_pack directory
+        //      - pack.mcmeta (need to rename from pack.mcmeta.json)
+        //      - pack.png
+        //      - assets (dir)
+
+        // 3. save all models (get from ModelManager)
+    }
 
 
     private static void saveModel(Model model) throws Exception {
